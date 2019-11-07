94bc12922a9bc7f946fcbf1a4c29c1614b10cc66
Diego Bordeu <dbordeu@uc.cl>
2019-11-07 11:29:15 -0300

    updated

--- filemerger.js
+++ filemerger.js
@@ -1,34 +1,43 @@
 // For reading .txt file code block
 const fs = require('fs');
+const path = require('path');
 
 const dir = '././log-backups/RS/oct31-nov7-2019/';
 
-const merger = () => {
-  const text1 = fs.readFileSync(`${dir}week1`).toString().split('\n');
-  console.log(text1.length);
-  const text2 = fs.readFileSync(`${dir}week2`).toString().split('\n');
-  console.log(text2.length);
-  const text3 = fs.readFileSync(`${dir}week3`).toString().split('\n');
-  console.log(text3.length);
-  const data = [text1, text2, text3];
-  const sum = [];
-  for (let i = 0; i < data.length; i++) {
-    for (let j = 0; j < data[i].length; j++) {
-      sum.push(data[i][j]);
-    }
-  }
-  return sum;
+
+// console.log(path.basename(dir));
+
+
+const merger = (folderDir) => { // eslint-disable-line
+  return new Promise((resolve, reject) => {
+    fs.readdir('././log-backups/RS/oct31-nov7-2019/', (err, files) => {
+      console.log(files);
+      if (err) return reject(new Error(err));
+      const data = [];
+      const sum = [];
+      for (let i = 0; i < files.length; i++) {
+        const text = fs.readFileSync(`${folderDir}${files[i]}`).toString().split('\n');
+        console.log(text.length);
+        data.push(text);
+      }
+      for (let i = 0; i < data.length; i++) {
+        for (let j = 0; j < data[i].length; j++) {
+          sum.push(data[i][j]);
+        }
+      }
+      return resolve(sum);
+    });
+  });
 };
 
-const main = async () => {
-  const sum = merger();
+const main = async (folderDir) => {
+  const sum = await merger(folderDir);
+  console.log('+++++++++++++ summary +++++++++++++++++');
   console.log(sum[0]);
   console.log(sum[sum.length - 100]);
   console.log(sum.length);
+  console.log('++++++++++++++++++++++++++++++++++++++');
+  return sum;
 };
 
-main().then(() => {
-  // console.log(a);
-}).catch((err) => {
-  console.log({ err });
-});
+main('././log-backups/RS/oct31-nov7-2019/');
